36257daec5137a1d6c0b09680a43477828bffad8
Bishal Changder <changdervishal1999@gmail.com>
2022-11-14 19:22:29 +0530

    First Commit. Date : 14/11/2022

---
+++ Arrays/InsertionArray.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int arr[10];
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // displaying array
+    for (int i = 0; i < 10; i++)
+    {
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
