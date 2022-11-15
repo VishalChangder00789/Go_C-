7de909145260590f658043543e0d7128d1d0ae4f
Bishal Changder <changdervishal1999@gmail.com>
2022-11-15 14:43:10 +0530

    Cummulative MaxSum SubArray TC: O(N^2). Date : 15/11/2022.

---
+++ Arrays/11-CummulativeMaxSumSubarray.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+// preprocessing
+int *cummulativeSumSubarray(int arr[], int n)
+{
+    int *cs = new int[n];
+
+    cs[0] = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        cs[i] = cs[i - 1] + arr[i];
+    }
+
+    return cs;
+}
+
+int cummulativeMaxSumSubArray(int arr[], int n, int cs[])
+{
+    int maxSum = 0;
+    int currentSum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            currentSum = cs[j] - cs[i - 1];
+            if (currentSum > maxSum)
+            {
+                maxSum = currentSum;
+            }
+        }
+    }
+
+    return maxSum;
+}
+
+int main()
+{
+    int arr[] = {-4, 1, 3, -2, 6, 2, -1, -4, -7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int *cs = cummulativeSumSubarray(arr, n);
+    int MaximumSum = cummulativeMaxSumSubArray(arr, n, cs);
+    cout << " Maximum Sum : " << MaximumSum << endl;
+}
