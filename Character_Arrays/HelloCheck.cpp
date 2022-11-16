4c46887e7ebfc2231757ca07dc5e87e5646686f4
Bishal Changder <changdervishal1999@gmail.com>
2022-11-16 12:41:56 +0530

    Better understanding. Date : 16/11/2022.

--- Character_Arrays/HelloCheck.cpp
+++ Character_Arrays/HelloCheck.cpp
@@ -3,5 +3,27 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World" << endl;
+    char a[10] = {'a', 'b', 'c'};
+    a[10] = '\0'; // It is very important to put a terminating character and make sure it is terminated.
+
+    cout << a << endl; // operator overaloding for << "left shift operator"  : Output content of the character array
+
+    char ah[] = {'a', 'b', 'c', 'd', 'e', '\0'};
+    char s1[] = {'h', 'e', 'l', 'l', 'o'};
+    char s2[] = "hello";
+
+    cout << ah << endl;               // prints abcde
+    cout << s1 << sizeof(s1) << endl; // prints helloabcde.
+    cout << s2 << sizeof(s2) << endl; // prints hello
+    cout << "================================================" << endl;
+    // Types of initialization
+    char a1[10];                                             // reserved for 10 character
+    char a2[10] = {'a', 'b', 'c', '\0', 'e', 'f', 'g', 'h'}; // Initialised with some characters
+    char a3[] = "hello";                                     // As a string : Null character added automatically
+    char a4[10] = "hello";                                   // has 5 initial characters as hello and then remaining 5 are empty
+
+    cout << a1 << sizeof(a1) << endl;
+    cout << a2 << sizeof(a2) << endl;
+    cout << a3 << sizeof(a3) << endl;
+    cout << a4 << sizeof(a4) << endl;
 }
