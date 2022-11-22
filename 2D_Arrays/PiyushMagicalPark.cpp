c33ebde32882af3cb77823fa64e83740b585103f
Bishal Changder <changdervishal1999@gmail.com>
2022-11-22 19:21:27 +0530

    Added Comments for better understanding. Date : 22/11/2022.

--- 2D_Arrays/PiyushMagicalPark.cpp
+++ 2D_Arrays/PiyushMagicalPark.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
 using namespace std;
 
+/*
+    Given a Matrix,
+
+    To be provided.
+    N= rows
+    M= cols
+    k= least strength needed to pass a row
+    s= total strength.
+
+
+    0. Provide values of M,N,K.S.
+    1. Take input of character array.  char arr[10][10].
+    2. Fill with the sample input.
+    3. Run the Function.
+
+    Condition :
+    Our players name is Piyush
+    Piyush needs to escape from a matrix with atleast k strength
+
+    How strength reduces?
+    Every step he takes forward from left to right ==> s-1
+    Every time he encounters a .  =>s-2;
+    Every time he encounters a * =>s+5;
+    Every time he encounters a # (blockade)=> switch to next line.
+
+    He does not looses any strength while changing the row or if he encounters # which results
+    in changing the row.
+
+    For each row he needs to have atleast k strength before switching to the next line.
+
+
+    return No for any false case
+    return Yes if he comes out.
+
+
+    Sample Input
+    4 4 5 20  M,N,K,S respectively.
+
+    !Input for character array
+    . . * .
+    . # . .
+    * * . .
+    . # * *
+
+*/
+
 string PiyushMagicalPark(char arr[][10], int m, int n, int s, int k)
 {
     for (int i = 0; i < m; i++)
