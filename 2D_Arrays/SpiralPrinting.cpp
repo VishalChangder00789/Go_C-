700c84232953de8feacbb53c45568bafbd42fb0a
Bishal Changder <changdervishal1999@gmail.com>
2022-11-22 17:11:49 +0530

    Started it

---
+++ 2D_Arrays/SpiralPrinting.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void SpiralTraversal(int arr[10][10], int m, int n)
+{
+}
+
+int main()
+{
+
+    int arr[10][10];
+    int m, n;
+    cin >> m >> n;
+
+    // Input
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    return 0;
+}
