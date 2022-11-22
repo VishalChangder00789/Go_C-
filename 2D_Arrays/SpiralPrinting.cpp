9bc0d2181036e847501ebf93fee278b2c91b65e3
Bishal Changder <changdervishal1999@gmail.com>
2022-11-22 18:37:22 +0530

    Sprial Matrix Printing. Date:22/11/2022.

--- 2D_Arrays/SpiralPrinting.cpp
+++ 2D_Arrays/SpiralPrinting.cpp
@@ -3,6 +3,49 @@ using namespace std;
 
 void SpiralTraversal(int arr[10][10], int m, int n)
 {
+    int sr = 0, sc = 0;
+    int er = m - 1;
+    int ec = n - 1;
+
+    while (sr <= er && sc <= ec)
+    {
+        // Top row
+        for (int i = sc; i <= ec; i++)
+        {
+            cout << arr[sr][i] << " , ";
+        }
+        sr++;
+        cout << endl;
+
+        // Right side
+        for (int i = sr; i <= er; i++)
+        {
+            cout << arr[i][ec] << " , ";
+        }
+        ec--;
+        cout << endl;
+
+        // Bottom Row
+        if (er > sr)
+        {
+            for (int i = ec; i >= sc; i--)
+            {
+                cout << arr[er][i] << " , ";
+            }
+            er--;
+        }
+        cout << endl;
+
+        if (ec > sc)
+        {
+            for (int i = er; i >= sr; i--)
+            {
+                cout << arr[i][sc] << " , ";
+            }
+            sc++;
+        }
+        cout << endl;
+    }
 }
 
 int main()
@@ -20,6 +63,8 @@ int main()
             cin >> arr[i][j];
         }
     }
+
+    SpiralTraversal(arr, m, n);
 
     return 0;
 }
