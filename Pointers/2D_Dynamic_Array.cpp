f2661c5180e614566a08ee1b703bb3fc2ccbcbd4
Bishal Changder <changdervishal1999@gmail.com>
2022-11-27 22:52:27 +0530

    How to create dynamic 2D arrays. Date:27/11/2022.

---
+++ Pointers/2D_Dynamic_Array.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int **arr; // double pointer
+    int row, col;
+
+    cin >> row >> col;
+
+    // Create row heads
+    for (int i = 0; i < row; i++)
+    {
+        arr[i] = new int[col]{0}; // each row contains the address of first block of coloumn arrays
+    }
+
+    // input
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    // output
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
