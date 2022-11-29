14ba7d295fe67429165d270050b1f1250f18d101
Bishal Changder <changdervishal1999@gmail.com>
2022-11-29 13:44:31 +0530

    Added Comments for better understanding. Date : 29/11/2022.

--- Numericals/5-HelpRahul.cpp
+++ Numericals/5-HelpRahul.cpp
@@ -7,6 +7,32 @@ using namespace std;
 
 // Simple : Find the key in a sorted and rotated array
 // variaton of binary search
+
+/**
+ * @brief
+ * Given an array : Sorted and Rotated.
+ * The two parts are in such a way that it makes the array unsorted.
+ * 4,5,6,1,2,3
+ * part 1: 4,5,6
+ * part 2 : 1,2,3
+ *
+ * If the mid lies in the first part for eg : 1st index is the mid,
+ * then the elements on the left of it are sorted in a decreasing fashion.
+ *
+ * If the mid lies in the second part for eg : 4th index then the element on the right of it
+ * are already sorted.
+ *
+ * We can directly check the key value and compare it to the region part1 and part2 sorted areas and
+ * find the elements.
+ *
+ * We can apply binary search on the right and left unsorted areas left to find the element.
+ *
+ * @param a : Sorted and rotated array
+ * @param n
+ * @param key
+ * @return int
+ */
+
 int HelpRahul(int a[], int n, int key)
 {
 
