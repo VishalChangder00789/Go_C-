a5d5e9751197ef7193c710bb6f7c57be42eedbd3
Bishal Changder <changdervishal1999@gmail.com>
2023-01-10 07:19:54 +0530

    Recursive Linear Search. Initial first complete commit. Date : 10/01/2023.

---
+++ Recursion_II_Implementation/LinearSearch.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int LinearSearch(int arr[], int n, int key, int i)
+{
+
+    if (arr[0] == key)
+    {
+        return i;
+    }
+
+    LinearSearch(arr + 1, n, key, i + 1);
+
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << LinearSearch(arr, n, 18, 0);
+}
