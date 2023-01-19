721aa6acaa2f5173e3bc6246c6ee9fcab0da865f
Bishal Changder <changdervishal1999@gmail.com>
2023-01-19 20:30:45 +0530

    _Basic Question : Array Reverse.

---
+++ Arrays/DSA_Sheet/ReverseArray.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void ReverseArrays(int arr[], int n)
+{
+
+    int i = 0;
+    int j = n - 1;
+
+    while (i < j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+    }
+}
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printArray(arr, n);
+    ReverseArrays(arr, n);
+    printArray(arr, n);
+}
