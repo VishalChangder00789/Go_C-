384904c141801828379b2d9e0b213a66a2d94021
Bishal Changder <changdervishal1999@gmail.com>
2023-01-19 17:46:14 +0530

    Variations added.

--- Pattern_Numericals/Pattern1.cpp
+++ Pattern_Numericals/Pattern1.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 using namespace std;
 
+void ReversePrinting()
+{
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 3; j >= 1; j--)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+void Pattern_2()
+{
+    int count = 1;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << count << " ";
+            count++;
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
 
@@ -12,4 +39,10 @@ int main()
         }
         cout << endl;
     }
+
+    cout << endl;
+    Pattern_2();
+    cout << endl;
+
+    ReversePrinting();
 }
