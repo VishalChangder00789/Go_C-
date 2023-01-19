d6aa7e24c20ac96cf3a0c79526780e8a0e8b24fe
Bishal Changder <changdervishal1999@gmail.com>
2023-01-19 19:45:58 +0530

    Patterns on asterik.

--- Pattern_Numericals/StarPatterns.cpp
+++ Pattern_Numericals/StarPatterns.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void countTriangle()
+void countTriangle(int a)
 {
     int count = 1;
 
@@ -9,15 +9,47 @@ void countTriangle()
     {
         for (int j = 0; j <= i; j++)
         {
-            cout << count;
+            cout << i + 1;
         }
         cout << endl;
         count++;
     }
 }
 
+void countTriangle(long int a)
+{
+    int cnt = 1;
+    for (int i = 0; i < 5; i++)
+    {
+        cnt = i;
+        for (int j = 0; j <= i; j++)
+        {
+            cout << cnt;
+            cnt++;
+        }
+        cout << endl;
+    }
+}
+
+void countTriangle(double a)
+{
+
+    int cnt = 1;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << cnt << "\t";
+            cnt++;
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
+
+    long int n = 23;
     /*
         *
         **
@@ -36,5 +68,9 @@ int main()
         cout << endl;
     }
 
-    countTriangle();
+    countTriangle(1);
+    cout << "========" << endl;
+    countTriangle(1.0);
+    cout << "========" << endl;
+    countTriangle(n);
 }
