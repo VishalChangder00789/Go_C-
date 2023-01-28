d3760e63cdefa5f51d54eb393a837e13a38d6aae
Bishal Changder <changdervishal1999@gmail.com>
2023-01-28 23:08:49 +0530

    __(ADDED BRUTE FORCE METHOD)__.

--- CodeBlocks/ArrayIntersection.cpp
+++ CodeBlocks/ArrayIntersection.cpp
@@ -2,6 +2,33 @@
 #include <vector>
 using namespace std;
 
+vector<int> ArrayIntersection_BruteForce(vector<int> arr1, int n, vector<int> arr2, int m)
+{
+    vector<int> ans;
+
+    for (int i = 0; i < n; i++)
+    {
+        int el = arr1[i];
+
+        for (int j = 0; j < m; j++)
+        {
+            if (el < arr2[j])
+            {
+                break;
+            }
+
+            if (el == arr2[j])
+            {
+                ans.push_back(el);
+                arr2[j] *= -1;
+                break;
+            }
+        }
+    }
+
+    return ans;
+}
+
 vector<int> ArrayIntersection(vector<int> arr1, int n, vector<int> arr2, int m)
 {
     vector<int> ans;
@@ -31,9 +58,10 @@ vector<int> ArrayIntersection(vector<int> arr1, int n, vector<int> arr2, int m)
 
 int main()
 {
-    vector<int> arr1{1, 2, 3, 4, 5};
-    vector<int> arr2{2, 4, 5, 7, 9};
-    vector<int> ans = ArrayIntersection(arr1, arr1.size(), arr2, arr2.size());
+    vector<int> arr1{1, 2, 2, 3, 4, 5};
+    vector<int> arr2{2, 2, 3, 3};
+    // vector<int> ans = ArrayIntersection(arr1, arr1.size(), arr2, arr2.size());
+    vector<int> ans = ArrayIntersection_BruteForce(arr1, arr1.size(), arr2, arr2.size());
     for (int i = 0; i < ans.size(); i++)
     {
         cout << ans[i] << " ";
