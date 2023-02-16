075c8964da017110bdc1ca19da5d5145782ee75b
Bishal Changder <changdervishal1999@gmail.com>
2023-02-16 08:56:50 +0530

    Leetcode 125. Valid Pallindrome. __ Added Love Babbar Code ,  __ Added Self Written Simplified code .

--- Leetcode/ValidPallindrome.cpp
+++ Leetcode/ValidPallindrome.cpp
@@ -1,3 +1,74 @@
+// Love Babbar Code :
+
+// class Solution
+// {
+// private:
+//     bool isValid(char ch)
+//     {
+//         if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9'))
+//         {
+//             return true;
+//         }
+
+//         return false;
+//     }
+
+//     char toLowerCase(char ch)
+//     {
+//         if ((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9'))
+//             return ch;
+//         else
+//         {
+//             char temp = ch - 'A' + 'a';
+//             return temp;
+//         }
+//     }
+
+//     bool checkPalindrome(string a)
+//     {
+//         int s = 0;
+//         int e = a.length() - 1;
+
+//         while (s <= e)
+//         {
+//             if (a[s] != a[e])
+//             {
+//                 return 0;
+//             }
+//             else
+//             {
+//                 s++;
+//                 e--;
+//             }
+//         }
+//         return 1;
+//     }
+
+// public:
+//     bool isPalindrome(string s)
+//     {
+//         // faltu character hatado
+//         string temp = "";
+
+//         for (int j = 0; j < s.length(); j++)
+//         {
+//             if (isValid(s[j]))
+//             {
+//                 temp.push_back(s[j]);
+//             }
+//         }
+
+//         // lowercase me kardo
+//         for (int j = 0; j < temp.length(); j++)
+//         {
+//             temp[j] = toLowerCase(temp[j]);
+//         }
+
+//         // check palindrome
+//         return checkPalindrome(temp);
+//     }
+// };
+
 #include <iostream>
 #include <string>
 using namespace std;
