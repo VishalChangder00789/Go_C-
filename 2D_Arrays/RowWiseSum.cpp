ec932c6d9683df01a2dd21c0b4d8383688e5a67f
Bishal Changder <changdervishal1999@gmail.com>
2023-02-22 08:13:45 +0530

    Added Coloumn wise Summation also .

--- 2D_Arrays/RowWiseSum.cpp
+++ 2D_Arrays/RowWiseSum.cpp
@@ -10,19 +10,21 @@ void printVector(vector<int> v)
     }
     cout << endl;
 }
-void _printMatrix(int a[10][10], int m, int n)
-{
-    cout << "Printing Square Matrix... " << endl;
 
-    for (int i = 0; i < m; i++)
+void _printMatrix(vector<vector<int>> a)
+{
+    cout << "Printing Vector... " << endl;
+
+    for (int i = 0; i < a.size(); i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < a[i].size(); j++)
         {
             cout << a[i][j] << " ";
         }
         cout << endl;
     }
 }
+
 void RowWiseSum(vector<vector<int>> a, int m, int n)
 {
     int sum = 0;
@@ -38,14 +40,39 @@ void RowWiseSum(vector<vector<int>> a, int m, int n)
         // v.push_back(sum);
         cout << sum << " ";
     }
-
+    cout << endl;
     // return v;
+}
+
+void ColWiseSum(vector<vector<int>> a, int m, int n)
+{
+    int sum = 0;
+    // vector<int> v;
+
+    for (int i = 0; i < m; i++)
+    {
+        sum = 0;
+        for (int j = 0; j < n; j++)
+        {
+            sum += a[j][i];
+        }
+        // v.push_back(sum);
+        cout << sum << " ";
+    }
+    cout << endl;
 }
 
 int main()
 {
-    vector<vector<int>> a = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}};
+    vector<vector<int>> a = {{1, 2, 3, 4, 5},
+                             {6, 7, 8, 9, 10},
+                             {11, 12, 13, 14, 15},
+                             {16, 17, 18, 19, 20},
+                             {21, 22, 23, 24, 25}};
     int m = 5, n = 5;
+
+    _printMatrix(a);
+    cout << endl;
 
     // vector<int> v = RowWiseSum(a, m, n);
     // printVector(v);
@@ -53,4 +80,5 @@ int main()
     // or
 
     RowWiseSum(a, m, n);
+    ColWiseSum(a, m, n);
 }
