796288c71bc6ee626293119764826ee8d08c8ab8
Bishal Changder <changdervishal1999@gmail.com>
2023-02-22 07:17:31 +0530

    Declaration of all the helper methods for 2D_Arrays.

---
+++ 2D_Arrays/Utilities_2D/Utils.h
@@ -0,0 +1,14 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include <vector>
+#include <iostream>
+using namespace std;
+
+// All Methods Declaration.
+
+void _takeInputs(int a[10][10], int m, int n);
+void _printMatrix(int a[10][10], int n);
+void _printMatrix(int a[10][10], int m, int n);
+void _printMatrix(vector<vector<int>> a, int m, int n);
+
+#endif
