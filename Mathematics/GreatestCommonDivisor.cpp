2d1f80c1c7b38992cf89f6169cdf1fb7a41743f9
Bishal Changder <changdervishal1999@gmail.com>
2023-03-04 12:22:33 +0530

    Greatest Common divisor using iterative approach .

---
+++ Mathematics/GreatestCommonDivisor.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if (a == 0)
+    {
+        return b;
+    }
+
+    if (b == 0)
+    {
+        return a;
+    }
+
+    while (a != b)
+    {
+        if (a > b)
+        {
+            a = a - b;
+        }
+        else
+        {
+            b = b - a;
+        }
+    }
+
+    return a;
+}
+int main()
+{
+
+    int a = 36, b = 60;
+    cout << gcd(a, b);
+}
