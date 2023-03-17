a1c58d8847b4c4a124d2e1c9a503c54713ef72a5
Bishal Changder <changdervishal1999@gmail.com>
2023-03-17 14:21:31 +0530

    Recursion and non Recursion method of Bubble Sort algorithm , Now i will never forget .

--- Recursion_BasicsToAdvanced/BubbleSort_Recursion.cpp
+++ Recursion_BasicsToAdvanced/BubbleSort_Recursion.cpp
@@ -3,6 +3,20 @@
 
 using namespace std;
 
+void BubbleSort_norm(vector<int> &v, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (v[j] > v[j + 1])
+            {
+                swap(v[j], v[j + 1]);
+            }
+        }
+    }
+}
+
 void BubbleSort(vector<int> &v, int n)
 {
 
@@ -28,7 +42,8 @@ int main()
     vector<int> v{10, 9, 2, 4, 8, 7, 1};
     int n = v.size();
 
-    BubbleSort(v, n);
+    // BubbleSort(v, n);
+    BubbleSort_norm(v, n);
 
     for (int i = 0; i < v.size(); i++)
     {
